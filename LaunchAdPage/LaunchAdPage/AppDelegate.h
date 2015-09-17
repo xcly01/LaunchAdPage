c6105f60d2dcd98ceb6c225f6b58f3b349c70ee3
liyang <493176452@qq.com>
2015-09-17 17:49:51 +0800

    commit LaunchAdPage

---
+++ LaunchAdPage/LaunchAdPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaunchAdPage
+//
+//  Created by liyang on 15/9/14.
+//  Copyright (c) 2015年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
